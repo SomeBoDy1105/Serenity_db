b5a6af2974c373ce9302ad0c1e3e85f7ce90a98d
Akram <bidka17@gmail.com>
2024-05-29 14:53:57 +0100

    create relation one to many between adviser and specialty

--- database/migrations/2024_05_29_022552_create_specialties_table.php
+++ database/migrations/2024_05_29_022552_create_specialties_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('specialties', function (Blueprint $table) {
             $table->id();
             $table->string('Nom_specialty');
-
             $table->timestamps();
         });
     }
